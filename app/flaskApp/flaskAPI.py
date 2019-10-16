88bffa6e54fa6246c3fd92fdfc5595c683c8f077
jeanmarcdav <jpd26362@email.vccs.edu>
2019-10-16 01:24:50 -0400

    Included jijna templating in dockerfile and base template

--- app/flaskApp/flaskAPI.py
+++ app/flaskApp/flaskAPI.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_restful import Resource, Api
 from config import Configuration
 
@@ -11,6 +11,10 @@ class HelloWorld(Resource):
         return {'hello': 'world'}
 
 api.add_resource(HelloWorld, '/')
-
+@app.route('/')
+@app.route('/test')
+# @app.route('/hello/<user>')
+def hello_world():
+    return render_template('base.html', user='tester')
 if __name__ == '__main__':
     app.run(host=cfg.host,port=cfg.port,debug=cfg.debug)

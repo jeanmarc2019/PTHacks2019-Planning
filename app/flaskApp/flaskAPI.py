84c15597b8e954f5f2b01c86abfd421b3809c00d
jeanmarcdav <jpd26362@email.vccs.edu>
2019-10-18 12:20:02 -0400

    Implemented swiping feature from codepen

--- app/flaskApp/flaskAPI.py
+++ app/flaskApp/flaskAPI.py
@@ -6,15 +6,15 @@ app = Flask(__name__)
 api = Api(app)
 cfg = Configuration(debug=True)
 
-class HelloWorld(Resource):
+class testEndpoint(Resource):
     def get(self):
         return {'hello': 'world'}
 
-api.add_resource(HelloWorld, '/')
+api.add_resource(testEndpoint, '/')
 @app.route('/')
 @app.route('/test')
 # @app.route('/hello/<user>')
-def hello_world():
-    return render_template('base.html', user='tester')
+def test_page():
+    return render_template('swiping.html')
 if __name__ == '__main__':
     app.run(host=cfg.host,port=cfg.port,debug=cfg.debug)

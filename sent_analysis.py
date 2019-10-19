9f768a79d49d043b12c366fd23ada7979ad1d515
warrie <warrie.proffitt@basecampcs.com>
2019-10-19 09:52:22 -0400

    init keywords branch:

--- sent_analysis.py
+++ sent_analysis.py
@@ -1,26 +1,71 @@
 import pandas, nltk
 from textblob import TextBlob
-from newspaper import Article
+from nltk.stem.porter import PorterStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
+import re
+
+#nltk.download('wordnet') 
+#nltk.download('stopwords')
+
+def sentAnalysis(dataset):
+    pos_sent = []
+    neg_sent = []
+    neutral = []
+
+    for index, row in dataset.iterrows():
+        
+        summary = row["Summary"]
+        body = row['Body']
+
+        if type(summary) is not float:
+            
+            sum_obj = TextBlob(summary)
+            sum_sentiment = sum_obj.sentiment.polarity
+
+            body_obj = TextBlob(body)
+            body_sentiment = body_obj.sentiment.polarity
+
+            print(sum_sentiment, body_sentiment)
+
+    return 0 
 
 
-df = pandas.read_csv('data.csv')
+dataset = pandas.read_csv('data.csv')
 
-pos_sent = []
-neg_sent = []
-neutral = []
 
-for index, row in df.iterrows():
+dataset['word_count'] = dataset['Body'].apply(
+    lambda x: len(str(x).split(" "))
+)
+
+
+stem = PorterStemmer()
+
+stop_words = set(stopwords.words("english"))
+
+corpus = []
+
+for i in range(0, 1933):
     
-    summary = row["Summary"]
-    body = row['Body']
+    text = str(dataset['Body'][i])
+    #print(text)
+    
+    text = re.sub('[^a-zA-Z]', ' ', text)
+    
+    
+    text = text.lower()
+    
+    text=re.sub("&lt;/?.*?&gt;"," &lt;&gt; ",text)
+    text=re.sub("(\\d|\\W)+"," ",text)
+    text = text.split()
+    
+    ps=PorterStemmer()
+    lem = WordNetLemmatizer()
+    text = [lem.lemmatize(word) for word in text if not word in  
+            stop_words] 
 
-    if type(summary) is not float:
-        
-        sum_obj = TextBlob(summary)
-        sum_sentiment = sum_obj.sentiment.polarity
-
-        body_obj = TextBlob(body)
-        body_sentiment = body_obj.sentiment.polarity
-
-        print(sum_sentiment, body_sentiment)
-        
+    text = " ".join(text)
+    corpus.append(text)
+    
+print(corpus[100])

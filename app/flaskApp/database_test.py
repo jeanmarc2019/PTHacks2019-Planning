d25fd60be691f457e7b1c3fa3264cc97f5065d63
jeanmarcdav <jpd26362@email.vccs.edu>
2019-10-19 18:01:57 -0400

    switched postgres to mysql and fixed create table for docker

--- app/flaskApp/database_test.py
+++ app/flaskApp/database_test.py
@@ -1,10 +1,15 @@
 from flaskAPI import app, db
+import mysql.connector
 import pandas, os
 from sqlalchemy import create_engine
 
-db_name = 'app.db'
-cur_dir = os.path.join(os.path.dirname(__file__), db_name)
-db_uri = 'sqlite:///{}'.format(cur_dir)
+docker = True
+if not docker:
+    db_name = 'app'
+    cur_dir = os.path.join(os.path.dirname(__file__), db_name)
+    db_uri = 'mysql:///{}'.format(cur_dir)
+else:
+    db_uri = 'mysql+mysqlconnector://root:tester@mysql/app'
 e = create_engine(db_uri)
 
 dataset = pandas.read_csv('data.csv')

76931e7ab5c74aadd8eabdfa459e272bab4bb10e
warrie <warrie.proffitt@basecampcs.com>
2019-10-19 16:20:43 -0400

    populated db

--- app/flaskApp/database_test.py
+++ app/flaskApp/database_test.py
@@ -1,6 +1,15 @@
 from flaskAPI import app, db
-import pandas
+import pandas, os
+from sqlalchemy import create_engine
+
+db_name = 'app.db'
+cur_dir = os.path.join(os.path.dirname(__file__), db_name)
+db_uri = 'sqlite:///{}'.format(cur_dir)
+e = create_engine(db_uri)
+
 dataset = pandas.read_csv('data.csv')
 
-for index, row in dataset.iterrows():
-    print(row['Body'])
+dataset.to_sql(name='dataset', con=e)
+
+#for index, row in dataset.iterrows():
+#    print(row['Body'])

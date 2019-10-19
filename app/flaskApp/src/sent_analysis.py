b961a94768c8bfb666e251e49f42b98aef09d2f4
warrie <warrie.proffitt@basecampcs.com>
2019-10-19 10:33:06 -0400

    added keywords to data.csv

--- app/flaskApp/src/sent_analysis.py
+++ app/flaskApp/src/sent_analysis.py
@@ -5,6 +5,7 @@ from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
 import re
+import numpy
 
 #nltk.download('wordnet') 
 #nltk.download('stopwords')
@@ -32,40 +33,45 @@ def sentAnalysis(dataset):
     return 0 
 
 
+def keywords(dataset):
+
+    dataset['word_count'] = dataset['Body'].apply(
+        lambda x: len(str(x).split(" "))
+    )
+
+
+    stem = PorterStemmer()
+
+    stop_words = set(stopwords.words("english"))
+
+    corpus = []
+
+    for i in range(0, 1933):
+        
+        text = str(dataset['Body'][i])
+        #print(text)
+        
+        text = re.sub('[^a-zA-Z]', ' ', text)
+        
+        
+        text = text.lower()
+        
+        text=re.sub("&lt;/?.*?&gt;"," &lt;&gt; ",text)
+        text=re.sub("(\\d|\\W)+"," ",text)
+        text = text.split()
+        
+        ps=PorterStemmer()
+        lem = WordNetLemmatizer()
+        text = [lem.lemmatize(word) for word in text if not word in  
+                stop_words] 
+
+        text = " ".join(text)
+        corpus.append(text)
+        
+    dataset['Corpus'] = corpus
+    dataset.to_csv('data.csv')
+
+    return 0
+
 dataset = pandas.read_csv('data.csv')
 
-
-dataset['word_count'] = dataset['Body'].apply(
-    lambda x: len(str(x).split(" "))
-)
-
-
-stem = PorterStemmer()
-
-stop_words = set(stopwords.words("english"))
-
-corpus = []
-
-for i in range(0, 1933):
-    
-    text = str(dataset['Body'][i])
-    #print(text)
-    
-    text = re.sub('[^a-zA-Z]', ' ', text)
-    
-    
-    text = text.lower()
-    
-    text=re.sub("&lt;/?.*?&gt;"," &lt;&gt; ",text)
-    text=re.sub("(\\d|\\W)+"," ",text)
-    text = text.split()
-    
-    ps=PorterStemmer()
-    lem = WordNetLemmatizer()
-    text = [lem.lemmatize(word) for word in text if not word in  
-            stop_words] 
-
-    text = " ".join(text)
-    corpus.append(text)
-    
-print(corpus[100])

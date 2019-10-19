fda6dbf5fabb4847e4b56c21618c4a11c250cee6
warrie <warrie.proffitt@basecampcs.com>
2019-10-19 00:27:41 -0400

    adding sent_anal and dat.csv

--- app/flaskApp/flaskAPI.py
+++ app/flaskApp/flaskAPI.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 from flask_restful import Resource, Api
 from config import Configuration
 
+import pandas
+
 app = Flask(__name__)
 api = Api(app)
 cfg = Configuration(debug=True)
@@ -13,8 +15,12 @@ class testEndpoint(Resource):
 api.add_resource(testEndpoint, '/')
 @app.route('/')
 @app.route('/test')
-# @app.route('/hello/<user>')
 def test_page():
-    return render_template('swiping.html')
+    data = pandas.read_csv("database_outline.csv")
+    data = data.to_dict('record')
+    print(data)
+    return render_template('swiping.html', data=data)
+
+
 if __name__ == '__main__':
     app.run(host=cfg.host,port=cfg.port,debug=cfg.debug)

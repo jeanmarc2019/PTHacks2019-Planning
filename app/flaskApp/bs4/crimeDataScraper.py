dd0084f5cf8212ba671bff95435cbac18ba1a562
JBen01 <jose.a.benavides01@gmail.com>
2019-10-20 04:04:16 -0500

    done

--- app/flaskApp/bs4/crimeDataScraper.py
+++ app/flaskApp/bs4/crimeDataScraper.py
@@ -3,6 +3,9 @@ import urllib.request
 import csv
 import re
 
+f = open("StateCrimeData.csv", 'w')
+ 
+
 
 sauce = urllib.request.urlopen('http://www.city-data.com/crime/')
 soup = bs.BeautifulSoup(sauce, 'lxml')
@@ -29,6 +32,15 @@ for a in states.find_all("a", href = True):
         s = " "
         tempCityName = s.join(finalCityName)
         finalCityName = tempCityName.split(" ")
+        CityOfficalName = ""
+        OfficialStateName = str(finalCityName[-1])
+        print("state: " + str(finalCityName[-1]))
+        if len(finalCityName) ==3:
+            CityOfficalName = str(finalCityName[0]) + "-" + str(finalCityName[1])
+        else:
+            CityOfficalName = str(finalCityName[0])
+
+        print("City Name: " + CityOfficalName)
 
 
         ##print("city: " + cityName[1])
@@ -36,7 +48,7 @@ for a in states.find_all("a", href = True):
         soup2 = bs.BeautifulSoup(sauce2, 'lxml')
         crimeData = soup2.find("div", {"class": "table-responsive"})
         years = [0000,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017]
-        crimeTypes = ["Murder"]
+        crimeAverages = []
         count = 0
 
         crime = crimeData.find("tfoot", {"class": "g8"})
@@ -45,8 +57,22 @@ for a in states.find_all("a", href = True):
             finalCrimeChart = str(tempCrime[1])
             tempCrime = str(finalCrimeChart).split("<")
             finalCrimeChart = str(tempCrime[0])
-            print("The Average crime rate is: 280.6 the cities crime rate for " + str(years[count]) + " is : " + str(finalCrimeChart)) 
-            count = count + 1
+            ##print("The Average crime rate is: 280.6 the cities crime rate for " + str(years[count]) + " is : " + str(finalCrimeChart)) 
+            crimeAverages.append(str(finalCrimeChart))
+        f.write("state:")
+        f.write(OfficialStateName + "\n")
+        f.write("city:")
+        f.write(CityOfficalName + "\n")
+        tempcount = 1
+        while tempcount < len(crimeAverages):
+            f.write("year:" + str(years[tempcount]) + ",crimerate:" + str(crimeAverages[tempcount])+"\n")
+            tempcount = tempcount + 1
+
+        """thewriter.writerow({"Types" : crimeAverages[0], "2003" : crimeAverages[1], "2004" : crimeAverages[2], "2005" : crimeAverages[3], "2006" : crimeAverages[4],
+        "2007" : crimeAverages[5], "2008" : crimeAverages[6], "2009" : crimeAverages[7], "2010" : crimeAverages[8], "2011" : crimeAverages[9],
+        "2012" : crimeAverages[10], "2013" : crimeAverages[11], "2014" : crimeAverages[12], "2015" : crimeAverages[13], "2016" : crimeAverages[14], 
+        "2017" : crimeAverages[15]})"""
+
 
 
         
@@ -70,6 +96,7 @@ for a in states.find_all("a", href = True):
                     pass
             count = count + 1
         """
+f.close()
     
 
 

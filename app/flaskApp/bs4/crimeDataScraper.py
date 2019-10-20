2242286e3afb0fd66db5d4eaeddcdffd2eb03652
JBen01 <jose.a.benavides01@gmail.com>
2019-10-20 01:08:46 -0500

    finalizing

--- app/flaskApp/bs4/crimeDataScraper.py
+++ app/flaskApp/bs4/crimeDataScraper.py
@@ -18,17 +18,36 @@ for a in states.find_all("a", href = True):
     cities = soup1.find("ul", {"class": "index"})
     for city in cities.find_all("a", href = True):
         cityName = str(city).split('"')
-        print("city: " + cityName[1])
+
+        tempCityName = str(cityName[1])
+        finalCityName = tempCityName.split("-")
+        finalCityName.remove("crime")
+        s = " "
+        tempCityName = s.join(finalCityName)
+        finalCityName = str(tempCityName).split(".")
+        finalCityName.remove("html")
+        s = " "
+        tempCityName = s.join(finalCityName)
+        finalCityName = tempCityName.split(" ")
+
+
+        ##print("city: " + cityName[1])
         sauce2 = urllib.request.urlopen("http://www.city-data.com/crime/" + str(cityName[1]))
         soup2 = bs.BeautifulSoup(sauce2, 'lxml')
         crimeData = soup2.find("div", {"class": "table-responsive"})
-        years = [2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017]
-        crimeTypes = ["Murder", ]
+        years = [0000,2003,2004,2005,2006,2007,2008,2009,2010,2011,2012,2013,2014,2015,2016,2017]
+        crimeTypes = ["Murder"]
         count = 0
 
-        crime = crimeData.find("tr", {"class": "g8"})
+        crime = crimeData.find("tfoot", {"class": "g8"})
         for crimes in crime.find_all("td"):
-            print(str(crimes))
+            tempCrime = str(crimes).split(">")
+            finalCrimeChart = str(tempCrime[1])
+            tempCrime = str(finalCrimeChart).split("<")
+            finalCrimeChart = str(tempCrime[0])
+            print("The Average crime rate is: 280.6 the cities crime rate for " + str(years[count]) + " is : " + str(finalCrimeChart)) 
+            count = count + 1
+
 
         
         """ while count < 8:
